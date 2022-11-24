ac0ff350fb36346adc96eda5e0cb504df5bdd6a3
hasan-naim <hasannaim6246@gmail.com>
2022-11-24 13:44:37 +0600

    api created to get catagories

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+/// midddlewares
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.49zsx7x.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function dbConnect() {
+  try {
+    const database = client.db("bookHouse");
+    const catagoriesCollection = database.collection("catagories");
+
+    app.get("/catagories", async (req, res) => {
+      const query = {};
+      const result = await catagoriesCollection.find(query).toArray();
+
+      res.send(result);
+    });
+  } catch (err) {
+    console.log(err, "error from try catch block");
+  }
+}
+
+dbConnect().catch((err) => console.log(err, "error from  dbconnect function"));
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
 app.listen(port, () => `server is running on ${port}`);
